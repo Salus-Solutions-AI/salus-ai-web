205cd205940a37163fa00b5bd27e21231a7107b7
Nolan McCafferty <njmccafferty@gmail.com>
2025-04-27 12:20:05 -0700

    fix config from rebase
    Signed-off-by: Nolan McCafferty <njmccafferty@gmail.com>

--- src/api/resources/default_categories.ts
+++ src/api/resources/default_categories.ts
@@ -3,6 +3,6 @@ import { Session } from '@supabase/supabase-js';
 import { Category } from '@/types';
 
 export const defaultCategoriesApi = {
-  getAll: (session: Session): Promise<Category[]> => 
+  getAll: (session: Session): Promise<Category[]> =>
     apiRequest('/api/default_categories', session),
 };
